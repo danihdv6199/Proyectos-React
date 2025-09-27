9671fd6413f4bad0cb3316410eafdf2d2b9af011
Daniel <dani6199@gmail.com>
2025-09-27 21:33:58 +0200

    Lista de tareas con UseState y Shadcn

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -1,11 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { TrafficLight } from './01-useState/TrafficLight'
+import { TasksApp } from './05-useReducer/TaskApp'
 import './index.css'
-import { TrafficLightWithEffect } from './\'2-useEffect/TrafficLightWithEffect'
-import { TrafficLightWitHook } from './\'2-useEffect/TrafficLightWithHook'
-import { PokemonPage } from './03-examples/PokemonPage'
-import { FocusScreen } from './04-useRef/FocusScreen'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -14,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWitHook /> */}
     {/* <PokemonPage /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <TasksApp />
   </StrictMode>,
 )
